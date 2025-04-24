583b12107820efa60285e71685933a6f9acf7f94
Faizahel Joasa Ariesta <faizaheljoasaariesta@gmail.com>
2025-04-25 03:47:23 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch';
+import net from 'net';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_KEY = process.env.API_KEY;
+const PROJECT_ID = process.env.PROJECT_ID;
+const DOCUMENT_PATH = process.env.DOCUMENT_PATH;
+
+const fetchGPS = async () => {
+    const url = `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/${DOCUMENT_PATH}?key=${API_KEY}`;
+
+    try {
+        const res = await fetch(url);
+        const json = await res.json();
+        const fields = json.fields;
+
+        const gpsData = {
+            lat: parseFloat(fields.lat.doubleValue),
+            lng: parseFloat(fields.lng.doubleValue),
+            speed: parseFloat(fields.speed.doubleValue),
+            altitude: parseFloat(fields.altitude.doubleValue),
+            satellites: parseInt(fields.satellites.integerValue),
+            course: parseFloat(fields.course.doubleValue),
+        };
+        return gpsData;
+    } catch (err) {
+        console.error("Gagal ambil data GPS:", err);
+        return null;
+    }
+};
+
+const sendToLabVIEW = (data) => {
+    const client = new net.Socket();
+    client.connect(6000, '192.168.100.84', () => {
+        const message = JSON.stringify(data);
+        client.write(message);
+        client.end();
+    });
+
+    client.on('error', (err) => {
+        console.error('TCP Error:', err.message);
+    });
+};
+
+setInterval(async () => {
+    const gps = await fetchGPS();
+    console.log(gps);
+    if (gps) sendToLabVIEW(gps);
+}, 3000);
+
