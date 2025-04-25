7d71f15f1d1132c3f6f0ac4511f89acb9265de74
Faizahel Joasa Ariesta <faizaheljoasaariesta@gmail.com>
2025-04-25 20:18:12 +0700

    update controller motor servo

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import fetch from 'node-fetch';
 import net from 'net';
 import dotenv from 'dotenv';
+import { Gpio } from 'pigpio';
 
 dotenv.config();
 
 const API_KEY = process.env.API_KEY;
 const PROJECT_ID = process.env.PROJECT_ID;
 const DOCUMENT_PATH = process.env.DOCUMENT_PATH;
+
+const servo = new Gpio(24, { mode: Gpio.OUTPUT });
 
 const fetchGPS = async () => {
     const url = `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/${DOCUMENT_PATH}?key=${API_KEY}`;
@@ -34,7 +37,7 @@ const fetchGPS = async () => {
 const sendToLabVIEW = (data) => {
     const client = new net.Socket();
     client.connect(6000, '192.168.100.84', () => {
-        const message = `${data.lat},${data.lng},${data.speed},${data.altitude},${data.satellites},${data.course}\n`;
+        const message = `${data.lat};${data.lng};${data.speed};${data.altitude};${data.satellites};${data.course}\n`;
         client.write(message);
         client.end();
     });
@@ -50,7 +53,44 @@ const sendToLabVIEW = (data) => {
 
 setInterval(async () => {
     const gps = await fetchGPS();
-    console.log(gps);
-    if (gps) sendToLabVIEW(gps);
-}, 3000);
+    if (gps) {
+        console.log('GPS:', gps);
+        sendToLabVIEW(gps);
+    }
+}, 1000);
 
+const server = net.createServer((socket) => {
+    console.log('LabVIEW terhubung ke server Raspberry Pi');
+
+    socket.on('data', (data) => {
+        const command = data.toString().trim();
+
+        const parts = command.split(";");
+
+        const status = parseInt(parts[0]);
+        const value = parseInt(parts[1]);
+
+        console.log(`Status Barrier: ${status}`);
+        console.log(`Value Barrier : ${value}`)
+
+        if (status === 1) {
+            const pulseWidth = 1000 + (value / 100) * 1000;
+            servo.servoWrite(pulseWidth);
+            console.log(`Servo dikendalikan ke posisi: ${pulseWidth} µs`);
+        } else {
+            console.log("Status bukan 1, servo tidak digerakkan");
+        }
+    });
+
+    socket.on('close', () => {
+        console.log('Koneksi LabVIEW ditutup');
+    });
+
+    socket.on('error', (err) => {
+        console.error('Socket Error:', err.message);
+    });
+});
+
+server.listen(6001, () => {
+    console.log('Server Raspberry Pi menunggu perintah di port 7000');
+});
